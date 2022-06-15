90cb10f3d47ff38d4e2627b2faf0aa4d0a7438ab
Miguel Angel Carrillo <carrilobravom@gmail.com>
2022-06-15 11:12:35 -0500

    Creacion

---
+++ stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Monkey 1", tipo: "arte digital", cantidad: 1, desc: "Bored Ape Yacht Club", precio: 1200, autor: "Rels", img: './img/monkey1.jpeg'},
+    {id: 2, nombre: "Monkey 2", tipo: "arte digital", cantidad: 1, desc: "Bored Ape Yacht Club", precio: 1100, autor: "Rels", img: './img/monkey2.jpeg'},
+    {id: 3, nombre: "Monkey 3", tipo: "arte digital", cantidad: 1, desc: "Bored Ape Yacht Club", precio: 1200, autor: "Rels", img: './img/monkey3.jpeg'},
+    {id: 4, nombre: "Monkey 4", tipo: "arte digital", cantidad: 1, desc: "Bored Ape Yacht Club", precio: 1400, autor: "Rels", img: './img/monkey4.jpeg'},
+    {id: 5, nombre: "Monkey 5", tipo: "arte digital", cantidad: 1, desc: "Bored Ape Yacht Club", precio: 1200, autor: "Rels", img: './img/monkey5.jpeg'},
+    {id: 6, nombre: "Invisible-Friend 1", tipo: "arte digital", cantidad: 1, desc: "YOU NEED AN INVISIBLE FRIEND", precio: 1200, autor: "Markus Magnusson", img: './img/InvisibleFriend.jpeg'},
+    {id: 7, nombre: "Invisible-Friend 2", tipo: "arte digital", cantidad: 1, desc: "YOU NEED AN INVISIBLE FRIEND", precio: 1100, autor: "Markus Magnusson", img: './img/InvisibleFriend2.jpeg'},
+    {id: 8, nombre: "Invisible-Friend 3", tipo: "arte digital", cantidad: 1, desc: "YOU NEED AN INVISIBLE FRIEND", precio: 1200, autor: "Markus Magnusson", img: './img/InvisibleFriend5.jpeg'},
+]
